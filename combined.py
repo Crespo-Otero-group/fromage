398a57f6ae2b2884f7ece5f1cd81ad2d14e2073f
Miguel Rivera <m.rivera@qmul.ac.uk>
2016-10-31 17:21:31 +0000

    Now interfacing with cp2k
    The program can now be put in a submission
    script and run cp2k calculations which yield
    positions and charge

--- combined.py
+++ combined.py
@@ -1,5 +1,5 @@
-#main function which takes in a vasp unit cell file
-
+# main function which takes in a vasp unit cell file
+# remember to module load cp2k
 import sys
 import numpy
 import subprocess
@@ -12,12 +12,9 @@ name="naphthalene222"
 
 vectors = readvasp(name)["vectors"]
 atoms= readvasp(name)["atoms"]
-print "hello"
-subprocess.call("./sleep.sh")
-print "boop"
+
 editcp2k(name, vectors,atoms)
-#subprocess.call("module load cp2k")
-#subprocess.call("cp2k.popt -i cp2k."+name+".in -o cp2k."+name+".out")
+#subprocess.call("cp2k.popt -i cp2k."+name+".in -o cp2k."+name+".out",shell=True)
 
 
 relaxedAtoms = readxyz(name+"-pos-1")[-1]
@@ -26,3 +23,4 @@ charges = readcp2kMull("cp2k."+name)
 
 for index, atom in enumerate(relaxedAtoms):
     atom.q = charges[index]
+    print st
