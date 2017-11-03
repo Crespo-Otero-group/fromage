49e613513568faff26d90c266360c8c43d6fe4ea
Miguel Rivera <m.rivera@qmul.ac.uk>
2017-11-03 16:55:28 +0000

    Fixed multiple atom labels check

--- parse_config_file.py
+++ parse_config_file.py
@@ -70,7 +70,7 @@ def parse_inputs(name="config"):
     inputs = complete_config(name)
 
     inputs["max_bl"] = float(inputs["max_bl"])
-    if type(inputs["atom_label"]):
+    if type(inputs["atom_label"]) == str:
         inputs["atom_label"] = [int(inputs["atom_label"]) - 1]
     else:
         label_atom = [int(i) - 1 for i in inputs["atom_label"]]
