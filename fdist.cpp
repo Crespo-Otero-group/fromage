84d0b8df4a9bc4f84caaea2e616eef3dcbbc5375
Miguel Rivera <m.rivera@qmul.ac.uk>
2018-01-16 10:36:32 +0000

    First cpp module

---
+++ fdist.cpp
@@ -0,0 +1,7 @@
+#include "dist.cpp"
+#include <cmath>
+
+double dist2(double x1, double y1, double z1, double x2, double y2, double z2) {
+        double out_dist = pow(x1-x2,2) + pow(y1-y2,2) + pow(z1-z2,2);
+        return out_dist;
+}
