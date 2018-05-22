43f7f259cbacec828ac781ad70f7020c209070a0
Miguel Rivera <m.rivera@qmul.ac.uk>
2018-05-22 11:12:48 +0100

    Test for smart distance

--- cryspy/tests/test_atom.py
+++ cryspy/tests/test_atom.py
@@ -1,5 +1,6 @@
 import pytest
 import cryspy.io.read_file as rf
+from pytest import approx
 from cryspy.utils.atom import Atom
 from cryspy.utils.mol import Mol
 
@@ -16,5 +17,9 @@ def h_at():
     return out_at
 
 def test_dist_to_atom(c_at, h_at):
-    """Distance to atom is working"""
+    """Distance to atom is implemented"""
     assert c_at.dist_at(h_at) == 1.3
+
+def test_smart_atom_distance(c_at, h_at):
+    """Smart distance is implemented"""
+    approx(c_at.smart_dist_at(h_at), 1.92)
