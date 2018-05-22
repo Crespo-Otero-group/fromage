7c58557dc0737e882c2ad501ea5ed3beb11b3546
Miguel Rivera <m.rivera@qmul.ac.uk>
2018-05-22 10:49:52 +0100

    Started tests on atom distance

---
+++ cryspy/tests/test_atom.py
@@ -0,0 +1,20 @@
+import pytest
+import cryspy.io.read_file as rf
+from cryspy.utils.atom import Atom
+from cryspy.utils.mol import Mol
+
+@pytest.fixture
+def c_at():
+    """Return a C Atom object at origin"""
+    out_at = Atom("C", 0.0, 0.0, 0.0)
+    return out_at
+
+@pytest.fixture
+def h_at():
+    """Return am O Atom object at x = 1.3"""
+    out_at = Atom("O", 1.3, 0.0, 0.0)
+    return out_at
+
+def test_dist_to_atom(c_at, h_at):
+    """Distance to atom is working"""
+    assert c_at.dist_at(h_at) == 1.3
