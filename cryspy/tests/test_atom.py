78ed305609c4baf2a5516dcd0b916943a14cb368
Miguel Rivera <m.rivera@qmul.ac.uk>
2018-05-24 16:06:14 +0100

    Improved test example for test_atom

--- cryspy/tests/test_atom.py
+++ cryspy/tests/test_atom.py
@@ -14,8 +14,8 @@ def c_at():
 
 @pytest.fixture
 def o_at():
-    """Return am O Atom object at x = 1.3"""
-    out_at = Atom("O", 1.3, 0.0, 0.0)
+    """Return am O Atom object at x = 0.8"""
+    out_at = Atom("O", 0.8, 0.0, 0.0)
     return out_at
 
 @pytest.fixture
@@ -27,17 +27,17 @@ def vectors():
 
 def test_dist_to_atom(c_at, o_at):
     """Distance to atom is implemented"""
-    assert c_at.dist_at(o_at) == approx(1.3)
+    assert c_at.dist_at(o_at) == approx(0.8)
 
 def test_at_lap(c_at, o_at):
     """Atom vdw overlap works"""
-    assert c_at.at_lap(o_at) == approx(1.92)
+    assert c_at.at_lap(o_at) == approx(2.42)
 
 def test_per_dist(c_at, o_at, vectors):
-    assert c_at.per_dist(o_at,vectors) == approx(0.3)
+    assert c_at.per_dist(o_at,vectors) == approx(0.2)
 
 def test_per_lap(c_at, o_at, vectors):
-    assert c_at.per_lap(o_at,vectors) == approx(2.92)
+    assert c_at.per_lap(o_at,vectors) == approx(3.02)
 
 def test_per_lap_img(c_at, o_at, vectors):
     r, new_at = c_at.per_lap(o_at,vectors,new_pos=True)
