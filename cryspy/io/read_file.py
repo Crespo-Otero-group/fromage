cb26fbef26163edb68813bb6fade40fb813efbe1
Miguel Rivera <m.rivera@qmul.ac.uk>
2018-09-24 11:00:17 +0100

    Read the correct unit of dftb out

--- cryspy/io/read_file.py
+++ cryspy/io/read_file.py
@@ -675,9 +675,9 @@ def read_dftb_out(in_name):
             if "Total Forces" in line:
                 read_grad = True
             if "Total energy" in line:
-                gr_energy = float(line.split()[4])
+                gr_energy = float(line.split()[2])
             if "Excitation Energy" in line:
-                exci += float(line.split()[1])
+                exci += float(line.split()[2])
 
     ex_energy = gr_energy + exci
 
