fc0744ab58482a5f8c8dec791cbb7bbc290c5899
Miguel Rivera <m.rivera@qmul.ac.uk>
2018-10-17 10:56:11 +0100

    Renamed to fromage in setup

--- setup.py
+++ setup.py
@@ -2,23 +2,23 @@
 
 from distutils.core import setup, Extension
 
-fdist_module = Extension('cryspy.fdist._fdist', sources=['cryspy/fdist/fdist_wrap.cxx', 'cryspy/fdist/fdist.cpp'],)
+fdist_module = Extension('fromage.fdist._fdist', sources=['fromage/fdist/fdist_wrap.cxx', 'fromage/fdist/fdist.cpp'],)
 
-setup(name='cryspy',
+setup(name='fromage',
       version='1.0',
       author='Miguel Rivera, Michael Dommett, Rachel Crespo-Otero',
       author_email='r.crespo-otero@qmul.ac.uk',
       ext_modules=[fdist_module],
-      packages=['cryspy',
-                'cryspy.fdist',
-                'cryspy.io',
-                'cryspy.scripts',
-                'cryspy.utils'],
-      scripts=['cryspy/scripts/assign_charges.py',
-               'cryspy/scripts/run_cryspy.py',
-               'cryspy/scripts/dimer_select.py',
-               'cryspy/scripts/pick_mol.py',
-               'cryspy/scripts/pop_stat.py',
-               'cryspy/scripts/prepare_calculation.py',
-               'cryspy/scripts/uc_tools.py'],
+      packages=['fromage',
+                'fromage.fdist',
+                'fromage.io',
+                'fromage.scripts',
+                'fromage.utils'],
+      scripts=['fromage/scripts/assign_charges.py',
+               'fromage/scripts/run_fromage.py',
+               'fromage/scripts/dimer_select.py',
+               'fromage/scripts/pick_mol.py',
+               'fromage/scripts/pop_stat.py',
+               'fromage/scripts/prepare_calculation.py',
+               'fromage/scripts/uc_tools.py'],
       )
