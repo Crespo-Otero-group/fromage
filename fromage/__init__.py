a3b0bff519befa0d9801f5cf1702688ceb675a60
Miguel Rivera <m.rivera@qmul.ac.uk>
2018-10-17 11:09:20 +0100

    updated __init__ fromage

---
+++ fromage/__init__.py
@@ -0,0 +1,14 @@
+"""
+cryspy
+
+General set of tools for manipulating and calculating molecular crystal.
+Existing electronic structure programs are interfaced with each other for
+ONIOM calculations with different levels of sophistication in the embedding.
+
+"""
+import sys
+import numpy as np
+from fromage import fdist
+from fromage import io
+from fromage import scripts
+from fromage import utils
