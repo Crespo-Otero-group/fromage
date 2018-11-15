13d9345d870fc5e8bc3ece6bad822a92a3c95964
Miguel Rivera <m.rivera@qmul.ac.uk>
2018-11-15 11:16:48 +0000

    Docstring typo

--- fromage/fdist/__init__.py
+++ fromage/fdist/__init__.py
@@ -4,7 +4,7 @@ Fast distance
 A distance calculator in C++. On certain machines, this can speed up the
 calculation of a proximity volume by three. For optimal performance, use
 fdist2 (the distance squared) rather than fdist if possible. This avoids the
-evaluation of unnecesary square roots.
+evaluation of unnecessary square roots.
 
 Module
 ------
