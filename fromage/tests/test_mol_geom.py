0567832f8204095fa68564eb3e1007a22a506024
Miguel Rivera <m.rivera@qmul.ac.uk>
2019-02-27 11:48:11 +0000

    Working on distance matrix

--- fromage/tests/test_mol_geom.py
+++ fromage/tests/test_mol_geom.py
@@ -3,4 +3,10 @@ import numpy as np
 
 def test_coord_array(h2o_dimer):
     arr = h2o_dimer.coord_array()
+    print(arr)
+    from sklearn.metrics.pairwise import pairwise_distances
+    print(pairwise_distances(arr))
     assert np.shape(arr) == (6, 3)
+
+def test_pairwise_distances2(h2o_dimer):
+    dis = h2o_dimer.pairwise_distances2()
--- fromage/utils/mol/_geom.py
+++ fromage/utils/mol/_geom.py
@@ -17,3 +17,25 @@ def coord_array(self):
         arr[i][1] = atom.y
         arr[i][2] = atom.z
     return arr
+
+def pairwise_distances2(self):
+    """
+    Return a numpy array of the pairwise squared distances
+
+    Returns
+    -------
+    arr : Nat x Nat numpy array
+        Squared distance matrix (lower triangular and 0 diagonal)
+
+    """
+
+    nat = len(self)
+    dis2 = np.zeros((nat,nat))
+    arr = self.coord_array()
+    for i, row_i in enumerate(arr):
+        for j, row_j in enumerate(arr[i+1:]):
+            squared_dist = np.sum(np.square(row_i-row_j))
+            dis2[j+i+1][i] = np.sqrt(squared_dist)
+            #print(np.sqrt(squared_dist))
+    print(dis2)
+    return
