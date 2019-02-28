74c1fcbd1b49d39d4b746b409f44c8083c460a43
Miguel Rivera <m.rivera@qmul.ac.uk>
2019-02-28 10:17:17 +0000

    Added test to extreme atom pairs

--- fromage/tests/test_mol_geom.py
+++ fromage/tests/test_mol_geom.py
@@ -6,5 +6,7 @@ def test_coord_array(h2o_dimer):
     assert np.shape(arr) == (6, 3)
 
 def test_extreme_at_pairs(h2o_dimer):
-    print(h2o_dimer.extreme_at_pairs(2))
-    return
+    res = h2o_dimer.extreme_at_pairs(2)
+    check = np.array([[[ 3.758602,0.5,0.504284],[ 0.,0.,0.,]],
+        [[ 3.758602,0.5,0.504284],[ 0.260455,0.,-0.872893]]])
+    assert (res == check).all()
