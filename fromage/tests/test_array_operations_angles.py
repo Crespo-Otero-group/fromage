5d90ea5d065a46548da7e7a0660f5bc74a63beba
Miguel Rivera <m.rivera@qmul.ac.uk>
2019-03-01 16:27:13 +0000

    Added vector angle test in radians

--- fromage/tests/test_array_operations_angles.py
+++ fromage/tests/test_array_operations_angles.py
@@ -7,3 +7,7 @@ from pytest import approx
 def test_angle_between_vectors(vec_100, vec_220):
     ang = ao.vec_angle(vec_100, vec_220)
     assert ang == approx(45.)
+
+def test_angle_between_vectors_rad(vec_100, vec_220):
+    ang = ao.vec_angle(vec_100, vec_220, degrees = False)
+    assert ang == approx(45.*np.pi/180)
