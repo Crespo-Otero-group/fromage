755fba364178831c593aef1e1f8ed5f876512d01
Miguel Rivera <m.rivera@qmul.ac.uk>
2019-03-01 10:45:26 +0000

    Used a fixture for h2o dimer

--- fromage/tests/test_dimer.py
+++ fromage/tests/test_dimer.py
@@ -2,10 +2,9 @@ import fromage.io.read_file as rf
 from fromage.utils.dimer import Dimer
 from fromage.tests.conftest import _in_data
 
-def test_init():
-    big_mol = rf.mol_from_file(_in_data("h2o_dimer.xyz"))
-    mol_a = big_mol.select(0)
-    mol_b = big_mol.select(2)
+def test_init(h2o_dimer):
+    mol_a = h2o_dimer.select(0)
+    mol_b = h2o_dimer.select(2)
     dim = Dimer([mol_a, mol_b])
     return
 
