5a5ac42af9cae45fce393ec267215016003cfe57
Miguel Rivera <m.rivera@qmul.ac.uk>
2019-03-04 14:23:50 +0000

    Testing dimers

--- fromage/tests/test_dimer.py
+++ fromage/tests/test_dimer.py
@@ -7,7 +7,9 @@ def test_init(h2o_dimer):
     mol_b = h2o_dimer.select(2)
     dim = Dimer([mol_a, mol_b])
     assert len(dim.mols[0]) == 3
+    assert len(dim.mols[1]) == 3
 
 def test_dim_from_file():
     dim = rf.dim_from_file(_in_data("h2o_dimer.xyz"))
-    return
+    assert len(dim.mols[0]) == 3
+    assert len(dim.mols[1]) == 3
