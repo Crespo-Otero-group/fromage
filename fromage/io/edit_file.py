Fixed issues with the functions to read hessians from Molcas and Orca. Added keywords to compute frequencies with Turbomole. Fixed the issues with SH dynamics with PyRAI2MD and NX with OpenMolcas

fromage/io/edit_file.py
@@ -227,9 +227,9 @@ def write_gauss(file_name, atoms, points, temp_name, proj_name='gaussian', freq=
                 line += ' \n'
                 if "freq=(SaveNormalModes)" not in line:
                     line += ' freq=(SaveNormalModes)'
-            if "&NSTATES" in line and states != None:
+            if "&NSTATES" in line and states is not None:
                 nstates = '%s' % (int(np.sum(states))-1)
-                if "&STATE" in line and state != None:
+                if "&STATE" in line and state is not None:
                     curr_state = '%s' % (state - 1)
                     modified_line = line.replace("&STATE", curr_state).replace("&NSTATES", nstates)
                 else:
@@ -250,6 +250,35 @@ def write_gauss(file_name, atoms, points, temp_name, proj_name='gaussian', freq=
         else:
             out_file.write(line)
     out_file.close()
+    return
+
+def write_dftb_dyn(file_name, 
+                   temp_name, 
+                   state: int, 
+                   states: list, 
+                   nac_coupling: list,
+                   soc_cupling: list
+                   point_flex: list):
+
+    with open(temp_name) as temp_file:
+        temp_content = temp_file.readlines()
+    
+    out_file = open(file_name, "w")
+
+    for line in temp_content:
+        if "&NSTATES" in line:
+            nstates = '%s' % ((int(np.sum(states))-1) + 10)
+            modified_line = line.replace("&NSTATES", nstates)
+            out_file.write(modified_line)
+        elif "&STATE" in line:
+            curr_state = '%s' % (state - 1)
+            modified_line = line.replace("&STATE",curr_state)
+            out_file.write(modified_line)
+        elif "&NAC" in line:
+            nac = '{%s %s}' % (0, nstates)
+            modified_line = line.replace("&NAC",nac)
+        else:
+            out_file.write(line)
     return
 
 def write_dftb(file_name, atoms, points, temp_name, proj_name='dftb',freq=None):
@@ -601,8 +630,6 @@ def write_molcas_free(file_name,
     for s, ns in enumerate(states):
         if isinstance(ns, list) and len(ns) > 0:
             ns = ns[0]  # Extract first number safely
-        else:
-            raise RuntimeError("Dynamics with singlets and triplets is not tested yet.")
         sub = []  # subsections of grad
         for n in range(ns):
             indx += 1
@@ -616,7 +643,6 @@ def write_molcas_free(file_name,
 
     # prepare nac section
     nac = [[] for x in nac_coupling] #  FJH Test this with internal modules for FSSH
-#    nac = [[] for x in grad]  # nac should have the same number of section as the grad
     if len(nac_coupling) > 0:
         for pair in nac_coupling:
             s1, s2 = pair  # two states

fromage/io/read_file.py
@@ -1102,7 +1102,7 @@ def MolcasCoord(M):
 
     return coord
 
-def read_molcas_ext(in_name, natom, state, states, mult, singlestate, soc_coupling):
+def read_molcas_ext(in_name, natom, state, states, mult, singlestate, soc_coupling, newtonx = None):
     """
     This function is used to read the Molcas output when the nonadiabatic dynamics 
     option is ON.
@@ -1138,6 +1138,17 @@ def read_molcas_ext(in_name, natom, state, states, mult, singlestate, soc_coupli
     sin_state  = 0
     tri_state  = 0
 
+    # PyRAI2MD gets the CI derivative coupling and then divides it by \DeltaE
+    # Eq 4 in [1]
+    # NewtonX gets the Total derivative coupling: CI_coup / DE + CSF_coup
+    # Eq 6 in [1]
+
+    # [1] J. Chem. Theory Comput. 2016, 12, 3636−3653
+
+    if newtonx is not None:
+        nacs_chain_str="""Total derivative coupling"""
+    else:
+        nacs_chain_str="""CI derivative coupling"""
 
     for i, line in enumerate(log):
         if   """Cartesian coordinates in Angstrom""" in line:
@@ -1152,7 +1163,10 @@ def read_molcas_ext(in_name, natom, state, states, mult, singlestate, soc_coupli
             else:
                 shift_line = 5  # relativistic energy output format
                 en_col = 1
-            e = [float(x.split()[en_col]) for x in log[i + shift_line: i + shift_line + states[spin][0]]]
+            if isinstance(states, list) and all(isinstance(item, list) for item in states):
+                e = [float(x.split()[en_col]) for x in log[i + shift_line: i + shift_line + states[spin][0]]]
+            else:
+                e = [float(x.split()[en_col]) for x in log[i + shift_line: i + shift_line + states[spin]]]
             casscf += e
 
         elif """Total CASPT2 energies:""" in line:
@@ -1163,7 +1177,10 @@ def read_molcas_ext(in_name, natom, state, states, mult, singlestate, soc_coupli
             else:
                 shift_line = 3  # relativistic energy output format FJH check this!!!!
                 en_col = 1
-            e = [float(x.split()[en_col]) for x in log[i + shift_line: i + shift_line + states[spin][0]]]
+            if isinstance(states, list) and all(isinstance(item, list) for item in states):
+                e = [float(x.split()[en_col]) for x in log[i + shift_line: i + shift_line + states[spin][0]]]
+            else:
+                e = [float(x.split()[en_col]) for x in log[i + shift_line: i + shift_line + states[spin]]]
             casscf += e
 
         elif """Total MS-CASPT2 energies:""" in line:
@@ -1192,8 +1209,9 @@ def read_molcas_ext(in_name, natom, state, states, mult, singlestate, soc_coupli
             g = log[i + 8: i + 8 + natom]
             g = S2F(g)
             gradient.append(g)
-                                                                           # FJH
-        elif """Total derivative coupling""" in line: #Added this for from&NX interface. It will cause conflicts
+
+        elif nacs_chain_str in line:
+#        elif """Total derivative coupling""" in line: #Added this for from&NX interface. It will cause conflicts
 #        elif """CI derivative coupling""" in line:   #with fro&PyRAI2MD which needs the CI der.. form. Fix this            
             n = log[i + 8: i + 8 + natom]
             n = S2F(n)
@@ -1364,8 +1382,7 @@ def read_dftb_out(in_name):
             if not line.strip():
                 read_grad = False
             if read_grad:
-#                atom_grads = [float(i) for i in line.split()]
-                atom_grads = [float(i) for i in line.split()[1:]] # FJH
+                atom_grads = [float(i) for i in line.split()[1:]] 
                 grad.extend(atom_grads)
             if "Total Forces" in line:
                 read_grad = True
@@ -1379,6 +1396,76 @@ def read_dftb_out(in_name):
     ex_energy = gr_energy + exci
 
     return ex_energy, grad, gr_energy
+
+def read_dftb_dyn(grad_file,
+                  ex_ener_file,
+                  nacs_file,
+                  natom,
+                  state,
+                  states,
+                  mult,
+                  nac_coupling,
+                  soc_coupling):
+
+    grad = []
+    energy = []
+    soc = []
+    nstates = np.sum(states)
+
+    with open(grad_file) as lines:
+        read_grad = False
+        for line in lines:
+            if not line.strip():
+                read_grad = False
+            if read_grad:
+                atom_grads = [float(i) for i in line.split()[1:]]
+                grad.extend(atom_grads)
+            if "Total Forces" in line:
+                read_grad = True
+            if "Total energy" in line:
+                gr_energy = float(line.split()[2])
+
+    energy.append(gr_energy)
+    gradient = np.zeros((nstates, natom, 3))
+    gradient[state - 1] = -np.array(grad)
+
+    with open(ex_ener_file) as lines:
+        content = lines.readlines()
+
+    for line in content[5:5+nstates]:
+        energy.append(float(line.split()[0]))
+
+    energy = np.array(energy)
+
+    nac = read_dftb_nac(nacs_file, natom)   
+
+    return energy, gradient, gr_energy, nac, soc
+
+def read_dftb_nac(nacs_file,natom):
+    """
+    read nac vectors from a td-dftb+ calculation
+
+    """
+    data = []
+    with open(nacs_file, 'r') as file:
+        lines = file.readlines()
+        current_batch = []
+        for line in lines:
+            parts = line.strip().split()
+            # Try to parse lines with exactly 3 float values
+            if len(parts) == 3:
+                try:
+                    floats = [float(p) for p in parts]
+                    current_batch.append(floats)
+                    if len(current_batch) == natom:
+                        data.append(current_batch)
+                        current_batch = []
+                except ValueError:
+                    continue  # skip lines that can't be converted
+            else:
+                continue
+
+    return np.array(data)
 
 def read_dftb_charges(in_name):
     """
@@ -2206,6 +2293,7 @@ def read_orca_out(in_name):
     with open(in_name) as Orca_file:
         content = Orca_file.readlines()
 
+    disp = 0.
     grad = []
     gr_energy=None
     state_energy=None
@@ -2220,17 +2308,24 @@ def read_orca_out(in_name):
         if "CARTESIAN GRADIENT" in line:
             orig_line = i
             break
+        if "Dispersion correction" in line:
+            disp = float(line.split()[2])
+
     for line in content[orig_line + 3:]:
         if len(line.split()) == 6:
             atom_grads = [float(i) for i in line.split()[3:]]
             grad = np.concatenate((grad,atom_grads))
         else:
             break
-#
+    
+    state_energy += disp
     if gr_energy == None:
         gr_energy = state_energy
-#
+    else:
+        gr_energy += disp
+
     return state_energy, grad, gr_energy
+
 ###################################################################
 #####################  Read Hessians  #############################
 """
@@ -2319,13 +2414,13 @@ def read_hessian_turbo(in_name):
             hess[j,i] = hess_tmp[cont]
     return hess
 #
-def read_hessian_molcas(in_name):
+def read_molcas_hessian(hess_file):
     """
     In Molcas, the hessian is obtained from a binary h5 file. Hence,
     the module h5py must be imported
     """
     import h5py
-    f = h5py.File(in_name, 'r')
+    f = h5py.File(hess_file, 'r')
     nuclei_key = list(f.keys())[0]
     hessian_key = list(f.keys())[-1]
     natoms = len(list(f[nuclei_key]))
@@ -2337,54 +2432,34 @@ def read_hessian_molcas(in_name):
     hess = np.zeros((hess_dim,hess_dim))
     # convert the lower triangle matrix to the symetric full Hessian
     hess[np.tril_indices(hess.shape[0], k = 0)] = hess_lt
+    hess = hess + hess.T - np.diag(hess.diagonal())
     return hess
-#
+
 def read_hessian_orca(in_name):
     """
     Read the hessian matrix computed by Orca
     """
+
     with open(in_name, 'r') as file:
-        lines = file.readlines()
+        lines = data.read().splitlines()
 
-    # Find the line index where $hessian is located
-#    hessian_index = -1
-#    for i, line in enumerate(lines):
-#        if "$hessian" in line:
-#            hessian_index = i
-#            break
+    hess = None
+    for n, line in enumerate(lines):
+        if '$hessian' in line:
+            nmode = int(lines[n + 1].split()[0])
+            nline = (nmode + 1) * (int(nmode / 5) + (nmode % 5 > 0))
+            vects = lines[n + 2: n + 2 + nline]
+            nmodes = [[] for _ in range(nmode)]
+            for m, i in enumerate(vects):
+                row = m % (nmode + 1) - 1
+                if row >= 0:
+                    nmodes[row] += [float(j) for j in i.split()[1:]]
+            hess = np.array(nmodes).T.reshape((nmode, nmode))
 
-#    if hessian_index == -1:
-#        raise ValueError(f"Hessian data not found in the file {file_path}")
-
-    hessian_index = next((i for i, line in enumerate(lines) if "$hessian" in line), None)
-    if hessian_index is None:
+    if hess is None:
         raise ValueError(f"Hessian data not found in the file {in_name}")
 
-    dim = int(lines[hessian_index + 1].strip())
-    hess = np.zeros((dim, dim))
-
-    current_row = 0
-    total_col_index = 0  # Total column index across all blocks
-
-    for line in lines[hessian_index + 2:]:
-        if line.strip(): 
-            values = [float(val) for val in line.split()[1:]]
-
-            for i, value in enumerate(values):
-                if total_col_index + i < dim:
-                    hess[current_row, total_col_index + i] = value
-
-            current_row += 1
-
-            if current_row == dim:
-                total_col_index += len(values)
-                current_row = 0
-
-                if total_col_index >= dim:
-                    break
-
     return hess
-
 
 """
  To implement: Q-Chem - NWChem - dftbplus

fromage/utils/calc.py
@@ -156,7 +156,9 @@ class DFTB_calc(Calc):
     Calculation of DFTB+ tested with v22.2
 
     """
-    def run(self, atoms, points_flex = None, nprocs=None):
+    def run(self, atoms, point_flex = None, nprocs = None, state=None,
+            states=None, singlestate=0, nac_coupling=[], soc_coupling=[]):
+#def run(self, atoms, points_flex = None, nprocs=None):
         """
         Runs a DFTB+ force calculation using a .xyz file 
         and return a subprocess.Popen
@@ -178,6 +180,14 @@ class DFTB_calc(Calc):
                             [], self.calc_name + ".temp")
         if points_flex is not None:
             ef.write_dftb_charges("charges.dat", points_flex)
+            if state is not None and states is not None:
+                ef.write_dftb_dyn('dftb_in.hsd', self.calc_name + ".temp", state,
+                                  states, nac_coupling, soc_coupling, point_flex)
+        else:
+            if state is not None and states is not None:
+                ef.write_dftb_dyn('dftb_in.hsd', self.calc_name + ".temp", state, states,
+                                  singlestate, nac_coupling, soc_coupling, [])
+                
         # Run DFTB+
         proc = subprocess.Popen("dftb+ > dftb_out", shell=True)
 
@@ -214,7 +224,19 @@ class DFTB_calc(Calc):
 
         return proc
 
-    def read_out(self, positions, in_mol=None, in_shell=None, natoms_flex=None):
+#    def read_out(self, positions, in_mol=None, in_shell=None, natoms_flex=None):
+    def read_out(self, 
+                 positions, 
+                 dyn_bool=False,
+                 in_mol=None, 
+                 in_shell=None, 
+                 natoms_flex=None,
+                 natoms = None,
+                 state = None,
+                 states = None,
+                 mult = [],
+                 singlestate = 0,
+                 soc_coupling = []):
         """
         Analyse a DFTB+ detailed.out file while printing geometry updates
 
@@ -242,27 +264,44 @@ class DFTB_calc(Calc):
         dftb_path = os.path.join(self.here, self.calc_name)
         os.chdir(dftb_path)
 
-        energy, gradients_bohr, scf_energy = rf.read_dftb_out("detailed.out")
+        nac = []
+        soc = []
+
+        if state is not None and states is not None:
+            energy, gradients_b, scf_energy, nac, soc = rf.read_dftb_dyn("detailed.out",
+                                                                         'EXC.DAT',
+                                                                         'NACV.dat',
+                                                                          natoms,
+                                                                          state,
+                                                                          states,
+                                                                          mult,
+                                                                          singlepoint,
+                                                                          nac_coupling,
+                                                                          soc_coupling)
+
+            # fix gradients units to Hartree/Angstrom
+            gradients = gradients_b * bohrconv
+        else:
+            energy, gradients_b, scf_energy = rf.read_dftb_out("detailed.out")
+            if natoms_flex is not None:
+                if int(len(positions)) <= int(3*natoms_flex): 
+                    dim_flex = int(len(positions) + 3. * natoms_flex)
+                    gradients = np.zeros(dim_flex)
+                else:
+                    # truncate gradients if too long and fix gradients units to Hartree/Angstrom
+                    gradients = np.zeros(len(positions))
+                # Fix gradients units to Hartree/Angstrom
+                gradients[:len(positions)] = gradients_b[:len(positions)] * bohrconv
+            else:
+                # Fix gradients units to Hartree/Angstrom
+                gradients = gradients_b[:len(positions)] * bohrconv
 
         # update the geometry log
-        if in_mol != None:
+        if in_mol is not None:
             self.update_geom(positions, in_mol, in_shell)
 
-        # truncate gradients if too long and fix gradients units to Hartree/Angstrom
-        if natoms_flex is not None:
-            if int(len(positions)) <= int(3*natoms_flex): 
-                dim_flex = int(len(positions) + 3. * natoms_flex)
-                gradients = np.zeros(dim_flex)
-            else:
-                gradients = np.zeros(len(positions))
-            # Fix gradients units to Hartree/Angstrom
-            gradients[:len(positions)] = gradients_bohr[:len(positions)] * bohrconv
-        else:
-            # Fix gradients units to Hartree/Angstrom
-            gradients = gradients_bohr[:len(positions)] * bohrconv
-
         os.chdir(self.here)
-        return (energy, gradients, scf_energy)
+        return (energy, gradients, scf_energy, nac, soc)
 
     def read_charges(self, pop = None):
         """
@@ -335,11 +374,14 @@ class DFTB_calc(Calc):
         os.chdir(self.here)
         return hess
 
+    #### ADD HERE the functions for read_trans_mu and read_oos
+
 class Gauss_calc(Calc):
     """
     Calculation with Gaussian 09/16
     """
-    def run(self, atoms, point_flex = None, nprocs = None, state=None, states=None, singlestate=0, nac_coupling=[], soc_coupling=[]):
+    def run(self, atoms, point_flex = None, nprocs = None, state=None, 
+            states=None, singlestate=0, nac_coupling=[], soc_coupling=[]):
         """
         Write a Gaussian input file and return a subprocess.Popen
 
@@ -357,14 +399,14 @@ class Gauss_calc(Calc):
         os.chdir(gauss_path)
 
         if point_flex is not None:
-            if state != None and states != None:
+            if state is not None and states is not None:
                 ef.write_gauss(self.calc_name + ".com", atoms,
                            point_flex, self.calc_name + ".temp", state = state, states = states)
             else:
                 ef.write_gauss(self.calc_name + ".com", atoms,
                            point_flex, self.calc_name + ".temp")
         else:
-            if state != None and states != None:
+            if state is not None and states is not None:
                 ef.write_gauss(self.calc_name + ".com", atoms,
                        [], self.calc_name + ".temp", state = state, states = states)
             else:
@@ -476,7 +518,7 @@ class Gauss_calc(Calc):
         else:
             energy, gradients_b, scf_energy = rf.read_fchk("gck.fchk")
             # update the geometry log
-            if in_mol != None:
+            if in_mol is not None:
                 self.update_geom(positions, in_mol, in_shell)                      
 #          
             # truncate gradients if too long and fix gradients units to Hartree/Angstrom
@@ -668,7 +710,7 @@ class Gauss_CAS_calc(Calc):
         grad_e = grad_e * bohrconv
         grad_g = grad_g * bohrconv
         # update the geometry log
-        if in_mol != None:
+        if in_mol is not None:
             self.update_geom(positions, in_mol, in_shell)
 
         # truncate gradients if too long
@@ -874,7 +916,7 @@ class Turbo_calc_TDDFT(Calc):
             else:
                 gradients = gradients_b[:len(positions)] * bohrconv
         # update the geometry log
-        if in_mol != None:
+        if in_mol is not None:
             self.update_geom(positions, in_mol, in_shell)
         # truncate gradients if too long
         gradients = gradients[:len(positions)]
@@ -1111,7 +1153,7 @@ class Turbo_calc_MP2(Calc):
 
 
         # update the geometry log
-        if in_mol != None:
+        if in_mol is not None:
             self.update_geom(positions, in_mol, in_shell)
 
         subprocess.call("rm CC*", shell=True)
@@ -1332,7 +1374,7 @@ class Turbo_calc(Calc):
                 gradients = gradients_b[:len(positions)] * bohrconv
 
         # update the geometry log
-        if in_mol != None:
+        if in_mol is not None:
             self.update_geom(positions, in_mol, in_shell)
  
         os.chdir(self.here)
@@ -1562,7 +1604,7 @@ class Turbo_SCF_calc(Calc):
         # fix gradients units to Hartree/Angstrom
         gradients = gradients_b * bohrconv
         # update the geometry log
-        if in_mol != None:
+        if in_mol is not None:
             self.update_geom(positions, in_mol, in_shell)
 
         # truncate gradients if too long
@@ -1777,7 +1819,8 @@ class Molcas_calc(Calc):
                  states = None,
                  mult = [],
                  singlestate = 0,
-                 soc_coupling = []):
+                 soc_coupling = [],
+                 newtonx = None):
         """
         Analyse a Molcas .log file while printing geometry updates
 
@@ -1824,7 +1867,8 @@ class Molcas_calc(Calc):
                                                                            states,
                                                                            mult,
                                                                            singlestate,
-                                                                           soc_coupling)
+                                                                           soc_coupling,
+                                                                           newtonx)
 
             # fix gradients units to Hartree/Angstrom
             gradients = gradients_b * bohrconv
@@ -1849,7 +1893,7 @@ class Molcas_calc(Calc):
             else:
                 gradients = gradients_b[:len(positions)] * bohrconv
 
-        if in_mol != None:
+        if in_mol is not None:
             self.update_geom(positions, in_mol, in_shell)
 
         os.chdir(self.here)
@@ -2081,7 +2125,7 @@ class xtb_calc(Calc):
         # Fix gradients units to Hartree/Angstrom
 #        gradients = gradients_bohr * bohrconv
         # update the geometry log
-        if in_mol != None:
+        if in_mol is not None:
             self.update_geom(positions, in_mol, in_shell)
 
 
@@ -2270,7 +2314,7 @@ class xtb_calc_gfnff(Calc):
         # Fix gradients units to Hartree/Angstrom
 #        gradients = gradients_bohr * bohrconv
         # update the geometry log
-        if in_mol != None:
+        if in_mol is not None:
             self.update_geom(positions, in_mol, in_shell)
 
         # truncate gradients if too long and fix gradients units to Hartree/Angstrom
@@ -2413,7 +2457,7 @@ class Qchem(Calc):
         # fix gradients units to Hartree/Angstrom
         gradients = gradients_b * bohrconv
         # update the geometry log
-        if in_mol != None:
+        if in_mol is not None:
             self.update_geom(positions, in_mol, in_shell)
 
         # truncate gradients if too long
@@ -2490,7 +2534,7 @@ class nwchem_calc_DFT(Calc):
         # fix gradients units to Hartree/Angstrom
         gradients = gradients_b * bohrconv
         # update the geometry log
-        if in_mol != None:
+        if in_mol is not None:
             self.update_geom(positions, in_mol, in_shell)
 
         # truncate gradients if too long
@@ -2580,7 +2624,7 @@ class fomo_ci_calc(Calc):
         # fix gradients units to Hartree/Angstrom
         gradients = gradients_b * bohrconv
         # update the geometry log
-        if in_mol != None:
+        if in_mol is not None:
             self.update_geom(positions, in_mol, in_shell)
 
         # truncate gradients if too long
@@ -2706,7 +2750,7 @@ class Orca_calc(Calc):
             gradients = gradients_bohr[:len(positions)] * bohrconv
 
         # update the geometry log
-        if in_mol != None:
+        if in_mol is not None:
             self.update_geom(positions, in_mol, in_shell)
 
         os.chdir(self.here)

fromage/utils/newtonx/fro_nx.py
@@ -23,13 +23,15 @@ def read_nx_control():
     This subrutine identifies if the Newton-X calculation is to compute a spectra
     or dynamics
     """
+    states = []
     try:
         with open("control.d", "r") as file:
             line = file.readline()
             info = line.split(',')
             natoms = int(info[0].strip())
-            states = [int(info[2].strip())]
-            nstates = int(np.sum(states))
+            states.append(int(info[2].strip()))
+#            states = [int(info[2].strip())]
+#            nstates = int(np.sum(states))
             state = int(info[3].strip())        
     except FileNotFoundError:
         try:
@@ -133,7 +135,7 @@ def parse_fro_input(inputs,states):
         spin = [int(x) for x in inputs["spin"]]
 
     mult = []
-    nstates = int(np.sum(states))
+#    nstates = int(np.sum(states))
 
     for n, s in enumerate(states):
         ms = int(spin[n] * 2 + 1)
@@ -392,7 +394,7 @@ def newtonx_sequence(inputs,natoms,states,state):
 
     in_pos, mol_atoms, shell_atoms = get_mol_shell_atoms(mol_file,shell_file,flex)
 
-    methods_wnacs = ['molcas'] # Extend this list to other methods that compute NACs
+    methods_wnacs = ['molcas', 'dftb'] # Extend this list to other methods that compute NACs
     pass_nac = []
 
     if high_level in methods_wnacs:
@@ -416,16 +418,26 @@ def newtonx_sequence(inputs,natoms,states,state):
     if flex:
         rl_en_gr = rl.read_out(in_pos,in_mol = mol_atoms,in_shell = shell_atoms,natoms_flex = ll_natoms)
         ml_en_gr = ml.read_out(in_pos[:dim_hl], natoms_flex = ll_natoms)
-        mh_en_gr = mh.read_out(in_pos, natoms_flex = ll_natoms, natoms = hl_natoms, state = state,
-                               states = states, mult = mult, singlestate = singlestate, 
-                               soc_coupling = soc_coupling) 
+        if high_level == 'molcas':
+            mh_en_gr = mh.read_out(in_pos, natoms_flex = ll_natoms, natoms = hl_natoms, state = state,
+                                   states = states, mult = mult, singlestate = singlestate,
+                                   soc_coupling = soc_coupling, newtonx = True)
+        else:
+            mh_en_gr = mh.read_out(in_pos, natoms_flex = ll_natoms, natoms = hl_natoms, state = state,
+                                   states = states, mult = mult, singlestate = singlestate,
+                                   soc_coupling = soc_coupling)
         
     else:
         rl_en_gr = rl.read_out(in_pos,in_mol = mol_atoms,in_shell = shell_atoms)
         ml_en_gr = ml.read_out(in_pos)
-        mh_en_gr = mh.read_out(in_pos, natoms_flex = ll_natoms, natoms = natoms, state = state,
-                               states = states, mult = mult, singlestate = singlestate,
-                               soc_coupling = soc_coupling)
+        if high_level == 'molcas':
+            mh_en_gr = mh.read_out(in_pos, natoms_flex = ll_natoms, natoms = natoms, state = state,
+                                   states = states, mult = mult, singlestate = singlestate,
+                                   soc_coupling = soc_coupling, newtonx = True)
+        else:
+            mh_en_gr = mh.read_out(in_pos, natoms_flex = ll_natoms, natoms = natoms, state = state,
+                                   states = states, mult = mult, singlestate = singlestate,
+                                   soc_coupling = soc_coupling)
 
     """ data format
     mh_en_gr